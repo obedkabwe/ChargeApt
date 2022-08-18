ce81d5893e0d311ae685d1935ed4ee3a1f43aadd
obedkabwe <kabweobed@gmail.com>
2022-08-17 22:28:39 -0500

    model, ennum,controller CREATED

---
+++ src/main/java/com/oberson/charge/ChargeApplication.java
@@ -0,0 +1,13 @@
+package com.oberson.charge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oberson/charge/ChargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oberson.charge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
